bd023d9bfe34eb273aa2ed59c01cdf65acf7a3f7
Paul Hultgren <pjhultgren5@gmail.com>
2023-02-14 12:18:08 -0800

    create topic group

---
+++ src/api/topic-group/controllers/topic-group.ts
@@ -0,0 +1,7 @@
+/**
+ * topic-group controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::topic-group.topic-group');
---
+++ src/api/topic-group/routes/topic-group.ts
@@ -0,0 +1,7 @@
+/**
+ * topic-group router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::topic-group.topic-group');
